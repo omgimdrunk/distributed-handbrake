a11f165cc222d926b36358553d5a7ff1674cfac8
eritch <eritch@localhost>
2011-05-04 22:47:32 -0400

    Basic class for getting/putting to FTP server

---
+++ src/basicFTP.py
@@ -0,0 +1,26 @@
+from ftplib import FTP
+
+class FTPConnect(object):
+    def __init__(self,host='localhost',port='21',username='anonymous',password=None):
+        self._host=host
+        self._port=port
+        self._username=username
+        self._password=password
+        self._ftp = FTP()
+        self._ftp.connect(self._host,self._port)
+        if self._username=='anonymous':
+            self._ftp.login()
+        else:
+            self._ftp.login(self._username,self._password)
+            
+    def download(self,filename,outfile):
+        self._ftp.retrbinary("RETR "+filename,outfile.write)
+        
+    def upload(self,filename,infile):
+        self._ftp.storbinary("STOR "+filename,infile.read)
+        
+    def change_directory(self,directory):
+        self._ftp.cwd(directory)
+        
+    def close_connection(self):
+        self._ftp.close()
